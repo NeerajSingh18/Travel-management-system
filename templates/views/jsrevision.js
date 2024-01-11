1387b07d3f013ba715d0c34f7a8ac56257206960
NeerajSingh18 <uic.21mca2388@gmail.com>
2024-01-11 12:56:19 +0530

    committed

---
+++ templates/views/jsrevision.js
@@ -0,0 +1,12 @@
+document.getElementById("button").onclick = function ()
+{
+    let a =parseInt( document.getElementById("num").value);
+
+let b=  parseInt(document.getElementById("num1").value);
+
+
+
+let sum=a+b;
+
+document.getElementById("result").innerHTML= "the sum is  hello " + sum ;
+}
